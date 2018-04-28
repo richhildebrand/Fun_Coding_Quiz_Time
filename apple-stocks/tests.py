564c1a0ebd918c58492cad66380bde994cc0921e
richhildebrand <richhildebrand@gmail.com>
2018-04-27 20:55:12 -0400

    ensure various test cases

--- apple-stocks/tests.py
+++ apple-stocks/tests.py
@@ -12,6 +12,16 @@ class OurTests(unittest.TestCase):
         stock_prices_yesterday = [10, 12, 5, 8, 11, 9]
         result = main.get_max_profit(stock_prices_yesterday)
         assert result == 6
- 
+
+    def test_WithoutUsing_MaxValue(self):
+        stock_prices_yesterday = [11, 22, 15, 8, 20, 3]
+        result = main.get_max_profit(stock_prices_yesterday)
+        assert result == 12
+
+    def test_WithoutUsing_MinValue(self):
+        stock_prices_yesterday = [10, 30, 5, 8, 20, 3]
+        result = main.get_max_profit(stock_prices_yesterday)
+        assert result == 20
+
 if __name__ == '__main__':
     unittest.main()
