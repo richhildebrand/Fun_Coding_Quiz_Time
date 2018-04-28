6de2f105dcc8d61a01186463664b4c09e7b59e0d
richhildebrand <richhildebrand@gmail.com>
2018-04-28 17:30:17 -0400

    allow overlap

--- hi-cal/main.py
+++ hi-cal/main.py
@@ -1,2 +1,32 @@
 def merge_ranges(meetings):
-    return []
+    numberOfMeetings = len(meetings)
+
+    busyDuring = []
+    busyDuring.append(meetings[0])
+
+    for outerIndex in range(1, numberOfMeetings):
+        outerStartTime = meetings[outerIndex][0]
+        outerEndTime = meetings[outerIndex][1]
+
+        didMerge = False
+        numberOfBusyTimeSlots = len(busyDuring)
+        for innerIndex in range(0, numberOfBusyTimeSlots):
+            innerStartTime = busyDuring[innerIndex][0]
+            innerEndTime = busyDuring[innerIndex][1]
+
+            canBeMerged = False
+            if outerStartTime <= innerStartTime <= outerEndTime: canBeMerged = True
+            if innerStartTime <= outerStartTime <= innerEndTime: canBeMerged = True
+
+            if (canBeMerged):
+                newMin = min(innerStartTime, outerStartTime)
+                newMax = max(innerEndTime, outerEndTime)
+
+                newTupple = (newMin, newMax)
+                busyDuring[innerIndex] = newTupple
+                didMerge = True
+                break
+
+        if not didMerge: busyDuring.append(meetings[outerIndex])
+    
+    return busyDuring
--- hi-cal/test_hiCalShould.py
+++ hi-cal/test_hiCalShould.py
@@ -3,13 +3,37 @@ import main
 
 
 class HiCalShould(unittest.TestCase):
-    def test_mergeRanges(self):
-        meetings = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+    def test_mergeTwoRanges(self):
+        meetings = [(1, 3), (2, 4)]
+        
+        result = main.merge_ranges(meetings)
+        
+        expectedResult = [(1, 4)]
+        self.assertSequenceEqual(result, expectedResult)
+
+    def test_keepTwoRanges(self):
+        meetings = [(1, 3), (7, 10)]
+        
+        result = main.merge_ranges(meetings)
+        
+        expectedResult = [(1, 3), (7, 10)]
+        self.assertSequenceEqual(result, expectedResult)
+
+    def test_mergeRanges_InOrder(self):
+        meetings = [(0, 1), (3, 5), (4, 8), (9, 10), (10, 12)]
         
         result = main.merge_ranges(meetings)
         
         expectedResult = [(0, 1), (3, 8), (9, 12)]
         self.assertSequenceEqual(result, expectedResult)
+
+    # def test_mergeRanges_OutOfOrder(self):
+    #     meetings = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+        
+    #     result = main.merge_ranges(meetings)
+        
+    #     expectedResult = [(0, 1), (3, 8), (9, 12)]
+    #     self.assertSequenceEqual(result, expectedResult)
 
 
 if __name__ == '__main__':
