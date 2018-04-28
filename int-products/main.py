eb7af42035348227ecb9f6f8aa62dcca2a3108f6
richhildebrand <richhildebrand@gmail.com>
2018-04-28 13:07:55 -0400

    3n time

--- int-products/main.py
+++ int-products/main.py
@@ -1,11 +1,20 @@
 def get_products_of_all_ints_except_at_index(arrayOfInts):
-    products = []
-
     numberOfInts = len(arrayOfInts)
-    for outsideIndex in range(0, numberOfInts):
-        products.append(1)
-        for insideIndex in range(0, numberOfInts):
-            if insideIndex != outsideIndex:
-                products[outsideIndex] = products[outsideIndex] * arrayOfInts[insideIndex]
+    products = [None] * numberOfInts
+
+    rollingProduct = 1
+    productsBeforeIndex = [None] * numberOfInts
+    for index in range(0, numberOfInts):
+        productsBeforeIndex[index] = rollingProduct
+        rollingProduct = rollingProduct * arrayOfInts[index]
+
+    rollingProduct = 1
+    productsAfterIndex = [None] * numberOfInts
+    for index in range(numberOfInts -1, -1, -1):
+        productsAfterIndex[index] = rollingProduct
+        rollingProduct = rollingProduct * arrayOfInts[index]
+
+    for index in range(0, numberOfInts):
+        products[index] = productsBeforeIndex[index] * productsAfterIndex[index]
 
     return products
