6d045a104bc45c59d6d5bd9908737bc90f37e30f
richhildebrand <richhildebrand@gmail.com>
2018-04-28 14:39:22 -0400

    allow for zero

--- highest-product/main.py
+++ highest-product/main.py
@@ -1,8 +1,24 @@
 def calculateHighestProduct(numbers):
     numberOfNumbers = len(numbers)
-    product = 1
+    max1 = 0
+    max2 = 0
+    max3 = 0
 
     for index in range(0, numberOfNumbers):
-        product *= numbers[index]
+        number = numbers[index]
+        if number > max3:
+            max1 = max2 
+            max2 = max3
+            max3 = number
+        elif number > max2:
+            max1 = max2
+            max2 = number
+        elif number > max1:
+            max2 = number
 
-    return product
+    maxAll = max1 * max2 * max3
+    max12 = max1 * max2
+    max13 = max1 * max3
+    max23 = max2 * max3
+
+    return max(maxAll, max12, max13, max23)
--- highest-product/test_highestProductShould.py
+++ highest-product/test_highestProductShould.py
@@ -8,6 +8,26 @@ class HighestProductShould(unittest.TestCase):
         product = main.calculateHighestProduct(input)
         assert product == 6
 
+    def test_workWithFiveNumbers(self):
+        input = [5, 3, 10, 2, 1]
+        product = main.calculateHighestProduct(input)
+        assert product == 150
+
+    def test_firstNumberIsZero(self):
+        input = [0, 3, 10]
+        product = main.calculateHighestProduct(input)
+        assert product == 30
+
+    def test_secondNumberIsZero(self):
+        input = [5, 0, 10]
+        product = main.calculateHighestProduct(input)
+        assert product == 50
+
+    def test_lastNumberIsZero(self):
+        input = [5, 1, 0]
+        product = main.calculateHighestProduct(input)
+        assert product == 5
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
