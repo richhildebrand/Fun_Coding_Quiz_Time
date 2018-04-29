74527cfe635d408193dcc3de69d0ddab0958311c
richhildebrand <richhildebrand@gmail.com>
2018-04-29 12:28:41 -0400

    bottom up!

--- old-coins/main.py
+++ old-coins/main.py
@@ -5,4 +5,17 @@ def numberOfCombos(amount, denominations):
         if amount % denominations[0] == 0: return 1
         else: return 0
 
-    return 0
+    numberOfCombinationsFor = {}
+    numberOfCombinationsFor[0] = 1
+
+    for coinValue in denominations:
+        for amountToCheck in range(coinValue, amount + 1):
+            difference = amountToCheck - coinValue
+            print()
+            print('coin:' + str(coinValue) + '   difference:' + str(difference) + '   amountToCheck:' + str(amountToCheck))
+            numberOfCombinationsForDifference = numberOfCombinationsFor.get(difference, 0)
+            numberOfCombinationsForCurrentAmount = numberOfCombinationsFor.get(amountToCheck, 0)
+            numberOfCombinationsFor[amountToCheck] = numberOfCombinationsForDifference + numberOfCombinationsForCurrentAmount
+            print('number of combinations for ' + str(amountToCheck) + ' is '+ str(numberOfCombinationsFor[amountToCheck]))
+
+    return numberOfCombinationsFor[amount]
--- old-coins/test_algorithm.py
+++ old-coins/test_algorithm.py
@@ -3,6 +3,16 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
+    def test_givenThreeCoins(self):
+        denominations = [1, 2, 3]
+        result = main.numberOfCombos(4, denominations)
+        self.assertEqual(result, 4)
+
+    def test_givenTwoCoins(self):
+        denominations = [1, 2]
+        result = main.numberOfCombos(10, denominations)
+        self.assertEqual(result, 6)
+
     def test_givenOneCoin_ThatWorks(self):
         denominations = [1]
         result = main.numberOfCombos(7, denominations)
@@ -17,11 +27,6 @@ class AlgorithmShould(unittest.TestCase):
         denominations = []
         result = main.numberOfCombos(5, denominations)
         self.assertEqual(result, 0)
-
-    # def test_givenExample(self):
-    #     denominations = [1,2,3]
-    #     result = main.numberOfCombos(44, denominations)
-    #     self.assertEqual(result, 44)
 
 
 if __name__ == '__main__':
