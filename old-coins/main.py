7ba1e60f5519ea154c346c1d1cf9d13ade23d142
richhildebrand <richhildebrand@gmail.com>
2018-04-29 11:11:16 -0400

    handle a few edge cases

--- old-coins/main.py
+++ old-coins/main.py
@@ -1,2 +1,8 @@
 def numberOfCombos(amount, denominations):
+    numberOfMonies = len(denominations)
+    if numberOfMonies == 0: return 0
+    if numberOfMonies == 1: 
+        if amount % denominations[0] == 0: return 1
+        else: return 0
+
     return 0
--- old-coins/test_algorithm.py
+++ old-coins/test_algorithm.py
@@ -3,10 +3,25 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_givenExample(self):
-        denominations = [1,2,3]
-        result = main.numberOfCombos(44, denominations)
-        self.assertEqual(result, 44)
+    def test_givenOneCoin_ThatWorks(self):
+        denominations = [1]
+        result = main.numberOfCombos(7, denominations)
+        self.assertEqual(result, 1)
+
+    def test_givenOneCoin_ThatDoesNotWork(self):
+        denominations = [10]
+        result = main.numberOfCombos(5, denominations)
+        self.assertEqual(result, 0)
+
+    def test_givenZeroCoins(self):
+        denominations = []
+        result = main.numberOfCombos(5, denominations)
+        self.assertEqual(result, 0)
+
+    # def test_givenExample(self):
+    #     denominations = [1,2,3]
+    #     result = main.numberOfCombos(44, denominations)
+    #     self.assertEqual(result, 44)
 
 
 if __name__ == '__main__':
