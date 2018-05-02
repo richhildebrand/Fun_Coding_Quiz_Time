35a7257075939ace3c7418e3ca93efa4ae0d07db
richhildebrand <richhildebrand@gmail.com>
2018-05-01 22:07:35 -0400

    no loops!

--- ok-eros/main.py
+++ ok-eros/main.py
@@ -1,22 +1,27 @@
 import sys
 
 def findIntersection(r1, r2):
-    lowestX = sys.maxsize
-    highestX = -sys.maxsize-1
-    lowestY = sys.maxsize
-    highestY = -sys.maxsize-1
+    # lowestX = sys.maxsize
+    # highestX = -sys.maxsize-1
+    # lowestY = sys.maxsize
+    # highestY = -sys.maxsize-1
 
-    for x in range(r1['left_x'], r1['left_x'] + r1['width'] + 1):
-        if r2['left_x'] <= x <= (r2['left_x'] + r2['width']):
-            print("x inside: " + str(x))
-            lowestX = min(lowestX, x)
-            highestX = max(highestX, x)
+    # for x in range(r1['left_x'], r1['left_x'] + r1['width'] + 1):
+    #     if r2['left_x'] <= x <= (r2['left_x'] + r2['width']):
+    #         print("x inside: " + str(x))
+    #         lowestX = min(lowestX, x)
+    #         highestX = max(highestX, x)
         
-    for y in range(r1['bottom_y'], r1['bottom_y'] + r1['height'] + 1):
-        if r2['bottom_y'] <= y <= (r2['bottom_y'] + r2['height']):
-            print("y inside: " + str(y))
-            lowestY = min(lowestY, y)
-            highestY = max(highestY, y)
+    # for y in range(r1['bottom_y'], r1['bottom_y'] + r1['height'] + 1):
+    #     if r2['bottom_y'] <= y <= (r2['bottom_y'] + r2['height']):
+    #         print("y inside: " + str(y))
+    #         lowestY = min(lowestY, y)
+    #         highestY = max(highestY, y)
+
+    highestX = min(r1['left_x'] + r1['width'], r2['left_x'] + r2['width'])
+    lowestX = max(r1['left_x'], r2['left_x'])
+    highestY = min(r1['bottom_y'] + r1['height'], r2['bottom_y'] + r2['height'])
+    lowestY = max(r1['bottom_y'], r2['bottom_y'])
 
     return {
         # Coordinates of bottom-left corner
