29a5152b1f9aca4fe72d954eebe868d571b76a53
richhildebrand <richhildebrand@gmail.com>
2018-05-01 22:24:59 -0400

    simplify logic

--- ok-eros/main.py
+++ ok-eros/main.py
@@ -1,15 +1,13 @@
 import sys
 
 def findIntersection(r1, r2):
-    if r1['left_x'] > r2['left_x'] + r2['width']: return {}
-    if r2['left_x'] > r1['left_x'] + r1['width']: return {}
-    if r1['bottom_y'] > r2['bottom_y'] + r2['height']: return {}
-    if r2['bottom_y'] > r1['bottom_y'] + r1['height']: return []
-
     highestX = min(r1['left_x'] + r1['width'], r2['left_x'] + r2['width'])
     lowestX = max(r1['left_x'], r2['left_x'])
     highestY = min(r1['bottom_y'] + r1['height'], r2['bottom_y'] + r2['height'])
     lowestY = max(r1['bottom_y'], r2['bottom_y'])
+
+    if highestX < lowestX: return {}
+    if highestY < lowestY: return {}
 
     return {
         # Coordinates of bottom-left corner
