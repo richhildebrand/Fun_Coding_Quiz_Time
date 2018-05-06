c8cd794a85cdbe71eaaa5ac483ccf48b84776e91
richhildebrand <richhildebrand@gmail.com>
2018-05-06 13:20:04 -0400

    faster, cheaper mode

--- temperature-guarantee/main.py
+++ temperature-guarantee/main.py
@@ -7,11 +7,12 @@ class TempTracker():
         self.minTemp = sys.maxsize
         self.maxTemp = -sys.maxsize -1
         self.meanOfTemps = None
-        self.modeOfTemps = None
+        self.modeOfTemps = []
 
         self.numberOfTemps = 0
         self.sumOfTemps = 0
-        self.allTemps = []
+        self.maxOccurancesOfTemp = 0
+        self.allTemps = {}
 
     def insert(self, newTemp):
         self.minTemp = min(self.minTemp, newTemp)
@@ -21,8 +22,15 @@ class TempTracker():
         self.sumOfTemps += newTemp
         self.meanOfTemps = self.sumOfTemps / self.numberOfTemps
 
-        self.allTemps.append(newTemp)
-        self.modeOfTemps = statistics.mode(self.allTemps)
+        newTempCount = self.allTemps[newTemp] = self.allTemps.get(newTemp, 0) + 1
+        print('new temp count:' + str(newTempCount), '   new temp:' + str(newTemp))
+        
+        if newTempCount > self.maxOccurancesOfTemp:
+            self.modeOfTemps = [newTemp]
+            self.maxOccurancesOfTemp = newTempCount
+        elif newTempCount == self.maxOccurancesOfTemp:
+            self.modeOfTemps.append(newTemp) 
+
 
 
     def get_max(self):
--- temperature-guarantee/test_algorithm.py
+++ temperature-guarantee/test_algorithm.py
@@ -3,7 +3,7 @@ import main as main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_withFour_easyNumbers(self):
+    def test_withOneMode(self):
         tempTracker = main.TempTracker()
         tempTracker.insert(10)
         tempTracker.insert(10)
@@ -13,7 +13,21 @@ class AlgorithmShould(unittest.TestCase):
         self.assertEqual(tempTracker.get_max(), 15)
         self.assertEqual(tempTracker.get_min(), 5)
         self.assertEqual(tempTracker.get_mean(), 10)
-        self.assertEqual(tempTracker.get_mode(), 10)
+        self.assertEqual(tempTracker.get_mode(), [10])
+
+    def test_withThreeModes(self):
+        tempTracker = main.TempTracker()
+        tempTracker.insert(10)
+        tempTracker.insert(20)
+        tempTracker.insert(30)
+        tempTracker.insert(30)
+        tempTracker.insert(20)
+        tempTracker.insert(10)
+
+        self.assertEqual(tempTracker.get_max(), 30)
+        self.assertEqual(tempTracker.get_min(), 10)
+        self.assertEqual(tempTracker.get_mean(), 20)
+        self.assertEqual(tempTracker.get_mode(), [30, 20, 10])
 
 
 if __name__ == '__main__':
