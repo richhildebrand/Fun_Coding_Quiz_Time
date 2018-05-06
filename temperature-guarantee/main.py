1e7e42beb6a7f709a8463adc0f617578f914953b
richhildebrand <richhildebrand@gmail.com>
2018-05-06 13:04:12 -0400

    faster mean

--- temperature-guarantee/main.py
+++ temperature-guarantee/main.py
@@ -8,14 +8,20 @@ class TempTracker():
         self.maxTemp = -sys.maxsize -1
         self.meanOfTemps = None
         self.modeOfTemps = None
+
+        self.numberOfTemps = 0
+        self.sumOfTemps = 0
         self.allTemps = []
 
     def insert(self, newTemp):
         self.minTemp = min(self.minTemp, newTemp)
         self.maxTemp = max(self.maxTemp, newTemp)
 
+        self.numberOfTemps += 1
+        self.sumOfTemps += newTemp
+        self.meanOfTemps = self.sumOfTemps / self.numberOfTemps
+
         self.allTemps.append(newTemp)
-        self.meanOfTemps = statistics.mean(self.allTemps)
         self.modeOfTemps = statistics.mode(self.allTemps)
 
 
