b51185e1b26a5c97355fcf8125ca25935ba9ec49
richhildebrand <richhildebrand@gmail.com>
2018-05-06 14:07:11 -0400

    first run through

--- superbalanced-tree/main.py
+++ superbalanced-tree/main.py
@@ -1,5 +1,22 @@
+def traverseBranch(node, currentDepth, maxDepth): 
+    currentDepth += 1
+    maxDepth = max(currentDepth, maxDepth)
+    if (node.left != None):
+        leftDepth = traverseBranch(node.left, currentDepth, maxDepth)
+        maxDepth = max(leftDepth, maxDepth)
+    if (node.right != None):
+        rightDepth = traverseBranch(node.right, currentDepth, maxDepth)
+        maxDepth = max(rightDepth, maxDepth)
+
+    return maxDepth
+
 def isSuperBalanced(tree):
-    return False
+    leftDepth = traverseBranch(tree.left, 0, 0)
+    print('leftDepth:' + str(leftDepth))
+    rightDepth = traverseBranch(tree.right, 0, 0)
+    print('rightDepth:' + str(rightDepth))
+
+    return abs(leftDepth - rightDepth) <= 1
 
 
 class BinaryTreeNode(object):
