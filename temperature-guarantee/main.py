7bb3a9296876e6d0e5b3b1a453e45f4c87a500e3
richhildebrand <richhildebrand@gmail.com>
2018-05-06 12:50:44 -0400

    init first draft

--- temperature-guarantee/main.py
+++ temperature-guarantee/main.py
@@ -1,2 +1,33 @@
-def gettingStarted():
-    return True
+import statistics
+import sys
+
+class TempTracker():
+
+    def __init__(self):
+        self.minTemp = sys.maxsize
+        self.maxTemp = -sys.maxsize -1
+        self.meanOfTemps = None
+        self.modeOfTemps = None
+        self.allTemps = []
+
+    def insert(self, newTemp):
+        self.minTemp = min(self.minTemp, newTemp)
+        self.maxTemp = max(self.maxTemp, newTemp)
+
+        self.allTemps.append(newTemp)
+        self.meanOfTemps = statistics.mean(self.allTemps)
+        self.modeOfTemps = statistics.mode(self.allTemps)
+
+
+    def get_max(self):
+        return self.maxTemp
+
+    def get_min(self):
+        return self.minTemp
+
+    def get_mean(self):
+        return self.meanOfTemps
+
+    def get_mode(self):
+        return self.modeOfTemps
+    
--- temperature-guarantee/test_algorithm.py
+++ temperature-guarantee/test_algorithm.py
@@ -1,11 +1,19 @@
 import unittest
-import main
+import main as main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_smoke(self):
-        result = main.gettingStarted()
-        self.assertTrue(result)
+    def test_withFour_easyNumbers(self):
+        tempTracker = main.TempTracker()
+        tempTracker.insert(10)
+        tempTracker.insert(10)
+        tempTracker.insert(5)
+        tempTracker.insert(15)
+
+        self.assertEqual(tempTracker.get_max(), 15)
+        self.assertEqual(tempTracker.get_min(), 5)
+        self.assertEqual(tempTracker.get_mean(), 10)
+        self.assertEqual(tempTracker.get_mode(), 10)
 
 
 if __name__ == '__main__':
