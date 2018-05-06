7d9807c2aef8416c2fe6df8693fedb2bb88698ca
richhildebrand <richhildebrand@gmail.com>
2018-05-06 13:53:46 -0400

    init tests

--- superbalanced-tree/main.py
+++ superbalanced-tree/main.py
@@ -1,2 +1,18 @@
-def gettingStarted():
-    return True
+def isSuperBalanced(tree):
+    return False
+
+
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
