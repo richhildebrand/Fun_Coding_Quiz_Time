d2eeab2de00da60aa6a2bdc802a56acee20e0a27
richhildebrand <richhildebrand@gmail.com>
2018-05-07 20:00:58 -0400

    failing tests when violating root node rules

--- valid-binary-tree/test_algorithm.py
+++ valid-binary-tree/test_algorithm.py
@@ -8,7 +8,7 @@ class AlgorithmShould(unittest.TestCase):
         result = main.isValidBinarySearchTree(root)
         self.assertTrue(result)
 
-    def test_findValidTree(self):
+    def test_returnTrue_whenNodesBalance(self):
         root = main.BinaryTreeNode(10)
         left1 = root.insert_left(5)
         left1.insert_left(1)
@@ -20,10 +20,9 @@ class AlgorithmShould(unittest.TestCase):
         right2.insert_right(22)
 
         result = main.isValidBinarySearchTree(root)
-
         self.assertTrue(result)
 
-    def test_findInValidTree(self):
+    def test_returnFalse_withHigherLeftNode(self):
         root = main.BinaryTreeNode(10)
         left1 = root.insert_left(5)
         left1.insert_left(1)
@@ -32,10 +31,35 @@ class AlgorithmShould(unittest.TestCase):
         right1 = root.insert_right(15)
         right1.insert_left(12)
         right2 = right1.insert_right(20)
-        right2.insert_left(22)
+        right2.insert_right(22)
 
         result = main.isValidBinarySearchTree(root)
+        self.assertFalse(result)
 
+    def test_returnFalse_withRightBranchLowerThanRoot(self):
+        root = main.BinaryTreeNode(10)
+        left1 = root.insert_left(5)
+        left1.insert_left(1)
+        left1.insert_right(7)
+
+        right1 = root.insert_right(15)
+        right1.insert_left(9)
+        right1.insert_right(17)
+
+        result = main.isValidBinarySearchTree(root)
+        self.assertFalse(result)
+
+    def test_returnFalse_withLeftBranchHigherThanRoot(self):
+        root = main.BinaryTreeNode(10)
+        left1 = root.insert_left(5)
+        left1.insert_left(1)
+        left1.insert_right(12)
+
+        right1 = root.insert_right(15)
+        right1.insert_left(12)
+        right1.insert_right(15)
+
+        result = main.isValidBinarySearchTree(root)
         self.assertFalse(result)
 
 
