fc337e8cabb0581507f087ed17463a94fe6087c3
richhildebrand <richhildebrand@gmail.com>
2018-05-08 18:58:42 -0400

    works with all right nodes

--- 10-2nd-largest-binary-tree-element/main.py
+++ 10-2nd-largest-binary-tree-element/main.py
@@ -1,2 +1,29 @@
-def gettingStarted():
-    return True
+def findSecondLargestElement(root):
+    keepLooking = True
+    secondHighestValue = None
+    currentNode = root
+    while keepLooking:
+
+        if currentNode.right:
+            keepLooking = True
+            secondHighestValue = currentNode.value
+            currentNode = currentNode.right
+            continue
+        else: keepLooking = False
+
+    return secondHighestValue
+
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
--- 10-2nd-largest-binary-tree-element/test_algorithm.py
+++ 10-2nd-largest-binary-tree-element/test_algorithm.py
@@ -1,11 +1,16 @@
 import unittest
-import main
+import main as main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_smoke(self):
-        result = main.gettingStarted()
-        self.assertTrue(result)
+    def test_findWithOnly_RightBranches(self):
+        root = main.BinaryTreeNode(10)
+        right1 = root.insert_right(15)
+        right2 = right1.insert_right(22)
+        right2.insert_right(25)
+
+        result = main.findSecondLargestElement(root)
+        self.assertEquals(result, 22)
 
 
 if __name__ == '__main__':
