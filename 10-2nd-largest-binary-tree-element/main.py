a57f95ed485810ad899568a4102f2a2c859e9d20
richhildebrand <richhildebrand@gmail.com>
2018-05-08 19:54:01 -0400

    traverse less deep down the left side

--- 10-2nd-largest-binary-tree-element/main.py
+++ 10-2nd-largest-binary-tree-element/main.py
@@ -1,19 +1,25 @@
 import sys
 
+def findLargestElement(root):
+    currentNode = root
+    while currentNode:
+        if currentNode.right: currentNode = currentNode.right
+        else: break
+
+    return currentNode.value
+
 def findSecondLargestElement(root):
     if not root.left and not root.right: raise Exception("must have two nodes to find second highest value")
 
-    highestValue = -sys.maxsize-1
-    secondHighestValue = -sys.maxsize-1
+    secondHighestValue = None
     currentNode = root
     while currentNode:
-        if currentNode.value >= highestValue:
-            secondHighestValue = highestValue
-            highestValue = currentNode.value
-        elif currentNode.value > secondHighestValue:
+        if currentNode.right: 
             secondHighestValue = currentNode.value
-
-        currentNode = currentNode.right or currentNode.left
+            currentNode = currentNode.right
+        elif currentNode.left: 
+            return findLargestElement(currentNode.left)
+        else: break
 
     return secondHighestValue
 
