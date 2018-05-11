c6f6a1d73f4ed512af681b554e6ec28b4191a870
richhildebrand <richhildebrand@gmail.com>
2018-05-11 16:08:49 -0400

    self all the things!

--- 07-temperature-guarantee/round2.py
+++ 07-temperature-guarantee/round2.py
@@ -7,18 +7,18 @@ class TempTracker():
     meanTemp = None
     modeTemp = None
 
-    def insert(newTemp):
-        maxTemp = max(newTemp, maxTemp)
-        minTemp = min(newTemp, minTemp)
+    def insert(self, newTemp):
+        self.maxTemp = max(newTemp, self.maxTemp)
+        self.minTemp = min(newTemp, self.minTemp)
 
-    def get_max():
-        return maxTemp
+    def get_max(self):
+        return self.maxTemp
 
-    def get_min()
-        return minTemp
+    def get_min(self):
+        return self.minTemp
 
-    def get_mean()
-        return meanTemp
+    def get_mean(self):
+        return self.meanTemp
 
-    def get_mode()
-        return modeTemp
+    def get_mode(self):
+        return self.modeTemp
