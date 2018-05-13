e099c191aa00c7f5559451359ba12660d5bf7a41
richhildebrand <richhildebrand@gmail.com>
2018-05-13 13:05:59 -0400

    find second largest value

--- 10-2nd-largest-binary-tree-element/round2.py
+++ 10-2nd-largest-binary-tree-element/round2.py
@@ -1,5 +1,20 @@
+def findLargestElement(rootNode):
+    node = rootNode
+    while node:
+        if node.right: node = node.right
+        else: return node.value
+
 def findSecondLargestElement(rootNode):
-    return 0
+    if not (rootNode.left or rootNode.right): raise ValueError("must have at least two nodes") 
+
+    secondLargetsValue = rootNode.value
+    node = rootNode
+    while node:
+        if node.right: 
+            secondLargetsValue = node.value
+            node = node.right
+        elif node.left: return findLargestElement(node.left)
+        else: return secondLargetsValue
 
 class BinaryTreeNode(object):
 
