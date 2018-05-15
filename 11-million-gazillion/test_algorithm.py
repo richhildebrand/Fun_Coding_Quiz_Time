f7db6849cde518697c0eb81ac14e36e81369c49e
richhildebrand <richhildebrand@gmail.com>
2018-05-14 21:07:26 -0400

    ensure is working!

--- 11-million-gazillion/test_algorithm.py
+++ 11-million-gazillion/test_algorithm.py
@@ -3,12 +3,39 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_storeGoogle(self):
+    def test_storeSite(self):
         crawler = main.Crawler()
         crawler.addSite('123')
 
-        expected = { '1': { '2': { '3': {'*'} } } }
+        expected = { '1': { '2': { '3': {'*': True } } } }
         self.assertDictEqual(expected, crawler.visited)
+
+    def test_storeManySites(self):
+        crawler = main.Crawler()
+        crawler.addSite('123')
+        crawler.addSite('12345')
+        crawler.addSite('abc')
+        crawler.addSite('axyz')
+
+
+        expected = \
+        { 
+            '1': { '2': { '3':
+                            { '*': True,
+                            '4': { '5': { '*': True } }
+                            }
+                        } 
+            }, 
+            'a': {
+                    'b':{ 'c': {'*': True } },
+                    'x':{ 'y': {'z': {'*': True } } }
+                }
+        }
+        self.assertDictEqual(expected, crawler.visited)
+        self.assertTrue(crawler.hasSite('123'))
+        self.assertTrue(crawler.hasSite('12345'))
+        self.assertTrue(crawler.hasSite('abc'))
+        self.assertTrue(crawler.hasSite('axyz'))
 
     def test_knowIfVisited(self):
         crawler = main.Crawler()
