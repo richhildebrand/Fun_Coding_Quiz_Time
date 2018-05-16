06b2c45cbfc17c429fddbadd7781560f5dec7490
richhildebrand <richhildebrand@gmail.com>
2018-05-16 17:27:27 -0400

    must adjust for zero indexing

--- 12-quick-int-check/main.py
+++ 12-quick-int-check/main.py
@@ -1,2 +1,18 @@
 def getNumberOfSteps(numbers, numberToFind):
+    maxIndex = len(numbers)
+    minIndex = 0
+
+    stepCount = 0
+    while maxIndex != minIndex:
+        print('maxIndex:' + str(maxIndex) + '   minIndex:' + str(minIndex))
+        stepCount += 1
+        midPoint = (minIndex + maxIndex) // 2
+        print('midPoint' + str(midPoint))
+        numberToCheck = numbers[minIndex]
+
+        if numberToCheck == numberToFind: return stepCount
+        elif numberToFind > numberToCheck: minIndex = midPoint
+        elif numberToFind < numberToCheck: maxIndex = midPoint 
+
+
     return -1
