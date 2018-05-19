86801609488568ccf8a226100945e732ec0fc93f
richhildebrand <richhildebrand@gmail.com>
2018-05-18 20:50:16 -0400

    start comparing words

--- 13-big-words/main.py
+++ 13-big-words/main.py
@@ -1,2 +1,18 @@
-def gettingStarted():
-    return True
+def steps_to_rotation_point(ascDictionary):
+    return 0
+
+def first_word_is_first_alphabetically(first_word, second_word):
+    first_word_letter_count = len(first_word)
+    second_word_letter_count = len(second_word)
+
+    index = 0
+    while index < first_word_letter_count \
+      and index < second_word_letter_count:
+
+        if first_word[index] == second_word[index]: 
+            index += 1
+            continue
+
+        return first_word[index] < second_word[index]
+
+    raise Exception('woops!')
--- 13-big-words/test_algorithm.py
+++ 13-big-words/test_algorithm.py
@@ -3,9 +3,9 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_smoke(self):
-        result = main.gettingStarted()
-        self.assertTrue(result)
+    def test_a_should_come_before_b(self):
+        firstWordIsFirst = main.first_word_is_first_alphabetically('a', 'b')
+        self.assertTrue(firstWordIsFirst)
 
 
 if __name__ == '__main__':
