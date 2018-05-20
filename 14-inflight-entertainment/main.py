5dcb4e8db8b56d67d7a6f622fe6afaf7a59ef5e1
richhildebrand <richhildebrand@gmail.com>
2018-05-20 09:39:31 -0400

    brute force method

--- 14-inflight-entertainment/main.py
+++ 14-inflight-entertainment/main.py
@@ -1,2 +1,10 @@
-def gettingStarted():
-    return True
+def two_movies_sum_length(flight_length, movie_lengths):
+    number_of_movies = len(movie_lengths)
+
+    for length_index in range(0, number_of_movies):
+        length = movie_lengths[length_index]
+        for next_length_index in range(length_index+1, number_of_movies):
+            next_length = movie_lengths[next_length_index]
+            if (length + next_length) == flight_length: return True
+
+    return False
--- 14-inflight-entertainment/test_algorithm.py
+++ 14-inflight-entertainment/test_algorithm.py
@@ -3,10 +3,33 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_smoke(self):
-        result = main.gettingStarted()
+    def test_find_valid_sums(self):
+        flight_length = 5
+        movie_lenghts = [3, 2]
+
+        result = main.two_movies_sum_length(flight_length, movie_lenghts)
         self.assertTrue(result)
 
+    def test_not_find_valid_sums(self):
+        flight_length = 5
+        movie_lenghts = [1, 2]
+
+        result = main.two_movies_sum_length(flight_length, movie_lenghts)
+        self.assertFalse(result)
+
+    def test_not_use_first_number_twice(self):
+        flight_length = 6
+        movie_lenghts = [3, 1]
+
+        result = main.two_movies_sum_length(flight_length, movie_lenghts)
+        self.assertFalse(result)
+
+    def test_not_use_last_number_twice(self):
+        flight_length = 6
+        movie_lenghts = [1, 3]
+
+        result = main.two_movies_sum_length(flight_length, movie_lenghts)
+        self.assertFalse(result)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
