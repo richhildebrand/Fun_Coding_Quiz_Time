e33604ddc92b55f464f4a67a281e414824d7faa4
richhildebrand <richhildebrand@gmail.com>
2018-05-24 09:50:59 -0400

    use a set!

--- 14-inflight-entertainment/round2.py
+++ 14-inflight-entertainment/round2.py
@@ -1,2 +1,9 @@
 def two_movies_sum_length(flight_length, movie_lenghts):
-    return
+    movie_times_available = set()
+
+    for movie_length in movie_lenghts:
+        difference = flight_length - movie_length
+        if difference in movie_times_available: return True
+        movie_times_available.add(movie_length)
+
+    return False
