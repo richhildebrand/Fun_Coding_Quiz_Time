fc8d47baa6b6581dd7a177a7f27a85530c22306f
richhildebrand <richhildebrand@gmail.com>
2018-05-24 10:01:25 -0400

    find our fibs!

--- 15-nth-fibonacci/round2.py
+++ 15-nth-fibonacci/round2.py
@@ -1,2 +1,12 @@
 def fib(fib_to_find):
-    return
+    if fib_to_find == 0: return 0
+
+    previous_number = 1
+    previous_previous_number = 0
+
+    for index in range(1, fib_to_find):
+        temp = previous_number
+        previous_number = previous_previous_number + previous_number
+        previous_previous_number = temp
+
+    return previous_number
