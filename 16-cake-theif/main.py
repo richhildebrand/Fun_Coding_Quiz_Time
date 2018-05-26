c85cae562da42550bdc6d6a958122b3accc4d553
richhildebrand <richhildebrand@gmail.com>
2018-05-26 11:02:36 -0400

    n lg(n) * n time

--- 16-cake-theif/main.py
+++ 16-cake-theif/main.py
@@ -1,2 +1,18 @@
 def max_duffel_bag_value(cake_tuples, capacity):
-    return 0
+
+    cake_tuples =   sorted(cake_tuples, key=lambda i: i[0]/i[1])
+    print(cake_tuples)
+
+    profit = 0
+    cake_tuples_index = 0
+    filled_capacity = 0
+    while(filled_capacity < capacity):
+        weight_of_cake, value_of_cake = cake_tuples[cake_tuples_index]
+
+        cakes_to_put_in_bag = (capacity - filled_capacity) // weight_of_cake
+        profit += cakes_to_put_in_bag * value_of_cake
+        filled_capacity += cakes_to_put_in_bag * weight_of_cake
+
+        cake_tuples_index += 1
+
+    return profit
