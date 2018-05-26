00ef270e4730c86a68c02d06b1d9c97ae1c9442f
richhildebrand <richhildebrand@gmail.com>
2018-05-26 12:05:09 -0400

    guard for 0 values

--- 16-cake-theif/main.py
+++ 16-cake-theif/main.py
@@ -3,11 +3,12 @@ def max_duffel_bag_value(cake_tuples, capacity):
     profits = {}
     profits[0] = 0
  
-    for current_capacity in range(1, capacity+1):
+    for current_capacity in range(0, capacity+1):
         max_profit_at_current_capacity = 0
 
         for weight, value in cake_tuples:
             if weight > current_capacity: continue
+            if weight == 0 and value > 0: return float('inf')
 
             profit_with_cake = value + profits[current_capacity - weight]
             max_profit_at_current_capacity = max(max_profit_at_current_capacity, profit_with_cake)
--- 16-cake-theif/test_algorithm.py
+++ 16-cake-theif/test_algorithm.py
@@ -3,6 +3,22 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
+    def test_should_handle_cakes_with_zero_weight_and_a_value(self):
+        cake_tuples = [(0, 1), (3, 90), (2, 15)]
+        capacity = 0
+
+        max_value = main.max_duffel_bag_value(cake_tuples, capacity)
+
+        self.assertEqual(max_value,  float('inf'))
+
+    def test_should_handle_cakes_with_zero_weight_and_no_value(self):
+        cake_tuples = [(0, 0), (3, 90), (2, 15)]
+        capacity = 0
+
+        max_value = main.max_duffel_bag_value(cake_tuples, capacity)
+
+        self.assertEqual(max_value,  0)  
+
     def test_should_buySix_threePoundCakes_andOne_twoPoundCakes(self):
         cake_tuples = [(7, 160), (3, 90), (2, 15)]
         capacity = 20
