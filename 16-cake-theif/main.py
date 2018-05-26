65f84b0559501e58d9e30d354ce0394eb3b71703
richhildebrand <richhildebrand@gmail.com>
2018-05-26 11:23:59 -0400

    failing test when the bag does not fill completely

--- 16-cake-theif/main.py
+++ 16-cake-theif/main.py
@@ -1,12 +1,13 @@
 def max_duffel_bag_value(cake_tuples, capacity):
 
+    number_of_cake_types = len(cake_tuples)
     cake_tuples =   sorted(cake_tuples, key=lambda i: i[0]/i[1])
     print(cake_tuples)
 
     profit = 0
     cake_tuples_index = 0
     filled_capacity = 0
-    while(filled_capacity < capacity):
+    while(filled_capacity < capacity and cake_tuples_index < number_of_cake_types):
         weight_of_cake, value_of_cake = cake_tuples[cake_tuples_index]
 
         cakes_to_put_in_bag = (capacity - filled_capacity) // weight_of_cake
--- 16-cake-theif/test_algorithm.py
+++ 16-cake-theif/test_algorithm.py
@@ -11,6 +11,14 @@ class AlgorithmShould(unittest.TestCase):
 
         self.assertEqual(max_value, 555)
 
+    def test_should_buyTwo_threePoundCakes_andOne_fivePoundCake(self):
+        cake_tuples = [(3, 40), (5, 70)]
+        capacity = 9
+
+        max_value = main.max_duffel_bag_value(cake_tuples, capacity)
+
+        self.assertEqual(max_value, 120)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
