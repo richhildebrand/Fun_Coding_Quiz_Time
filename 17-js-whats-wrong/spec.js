9e4ae6ff2d2c9c10d27cceb640a8d179d1a0c121
richhildebrand <richhildebrand@gmail.com>
2018-05-28 10:06:57 -0400

    failing test - btnNum is undefined!

--- 17-js-whats-wrong/spec.js
+++ 17-js-whats-wrong/spec.js
@@ -1,9 +1,23 @@
 describe("Test method should", function() {
-  
-    it("return given word", function() {
-        var word = 'foo-bar';
-        let result = testMethod(word);
-        expect(result).toBe(word);
+    let _dom = undefined;
+
+    beforeEach(function() {
+        _dom = document.createElement("section");
+        _addButton(_dom, 'btn-0', 'Button 1!')
+        _addButton(_dom, 'btn-1', 'Button 2!')
+        _addButton(_dom, 'btn-2', 'Button 3!')
+    });
+
+    let _addButton = function(parent, id, text) {
+        var button = document.createElement("button");
+        button.setAttribute("id", id);
+        button.innerHTML = text;
+        parent.appendChild(button);
+    } 
+
+    it("click our first button", function() {
+        attachListeners(_dom)
+        _dom.querySelector('btn-0' + btnNum).click();
     });
 });
   
