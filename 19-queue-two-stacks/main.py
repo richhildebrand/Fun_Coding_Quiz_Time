b6be31cd7689235740c84fb70760d4a371f5925b
richhildebrand <richhildebrand@gmail.com>
2018-06-02 16:56:50 -0400

    init one queue with two stacks

--- 19-queue-two-stacks/main.py
+++ 19-queue-two-stacks/main.py
@@ -1,2 +1,13 @@
-def gettingStarted():
-    return True
+class MyQueue():
+
+    def __init__(self):
+        stack_one = []
+        stack_two = []
+
+    def enqueue(self, item):
+        return
+
+    def dequeue(self):
+        steps = 0
+        return (0, steps)
+    
--- 19-queue-two-stacks/test_algorithm.py
+++ 19-queue-two-stacks/test_algorithm.py
@@ -4,8 +4,14 @@ import main
 
 class AlgorithmShould(unittest.TestCase):
     def test_smoke(self):
-        result = main.gettingStarted()
-        self.assertTrue(result)
+        queue = main.MyQueue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        result, steps = queue.dequeue()
+        self.assertEqual(result, 1)
+        self.assertEqual(steps, 3)
 
 
 if __name__ == '__main__':
