694f9751bd736f8c21d3a03dfd3726ac12147a76
richhildebrand <richhildebrand@gmail.com>
2018-06-02 17:04:38 -0400

    ensure two dequeues in a row work

--- 19-queue-two-stacks/test_algorithm.py
+++ 19-queue-two-stacks/test_algorithm.py
@@ -3,15 +3,28 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_smoke(self):
+    def test_work_with_one_dequeue(self):
         queue = main.MyQueue()
         queue.enqueue(1)
         queue.enqueue(2)
         queue.enqueue(3)
 
         result, steps = queue.dequeue()
+
         self.assertEqual(result, 1)
         self.assertEqual(steps, 3)
+
+    def test_work_with_two_dequeues_in_a_row(self):
+        queue = main.MyQueue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        queue.dequeue()
+        result, steps = queue.dequeue()
+
+        self.assertEqual(result, 2)
+        self.assertEqual(steps, 0)
 
 
 if __name__ == '__main__':
