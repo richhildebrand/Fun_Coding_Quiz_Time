6fbc68a71618917beca7c7d359688edca9542f52
richhildebrand <richhildebrand@gmail.com>
2018-06-02 17:05:46 -0400

    test for dequeue with enqueue in the middle

--- 19-queue-two-stacks/test_algorithm.py
+++ 19-queue-two-stacks/test_algorithm.py
@@ -26,6 +26,19 @@ class AlgorithmShould(unittest.TestCase):
         self.assertEqual(result, 2)
         self.assertEqual(steps, 0)
 
+    def test_work_with_two_dequeues_out_of_order(self):
+        queue = main.MyQueue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        queue.dequeue()
+        queue.enqueue(4)
+        result, steps = queue.dequeue()
+
+        self.assertEqual(result, 2)
+        self.assertEqual(steps, 0)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
