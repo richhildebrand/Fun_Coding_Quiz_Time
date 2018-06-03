1265ae8f84eb9085f8f513061141315a5aa53455
richhildebrand <richhildebrand@gmail.com>
2018-06-03 12:33:07 -0400

    working with an empty stack

--- 20-largest-stack/main.py
+++ 20-largest-stack/main.py
@@ -1,2 +1,32 @@
-def gettingStarted():
-    return True
+class Stack(object):
+
+    def __init__(self):
+        """Initialize an empty stack"""
+        self.items = []
+
+    def push(self, item):
+        """Push new item to stack"""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return last item"""
+        # If the stack is empty, return None
+        # (it would also be reasonable to throw an exception)
+        if not self.items:
+            return None
+
+        return self.items.pop()
+
+    def peek(self):
+        """See what the last item is"""
+        if not self.items:
+            return None
+        return self.items[-1]
+
+class MaxStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.max_number_in_stack = None
+
+    def get_max(self):
+        return self.max_number_in_stack
--- 20-largest-stack/test_algorithm.py
+++ 20-largest-stack/test_algorithm.py
@@ -3,37 +3,10 @@ import main
 
 
 class AlgorithmShould(unittest.TestCase):
-    def test_smoke(self):
-        result = main.gettingStarted()
-        self.assertTrue(result)
-
-
-
-class Stack(object):
-
-    def __init__(self):
-        """Initialize an empty stack"""
-        self.items = []
-
-    def push(self, item):
-        """Push new item to stack"""
-        self.items.append(item)
-
-    def pop(self):
-        """Remove and return last item"""
-        # If the stack is empty, return None
-        # (it would also be reasonable to throw an exception)
-        if not self.items:
-            return None
-
-        return self.items.pop()
-
-    def peek(self):
-        """See what the last item is"""
-        if not self.items:
-            return None
-        return self.items[-1]
-
+    def test_find_None_when_no_items_are_in_stack(self):
+        stack = main.MaxStack()
+        max_value = stack.get_max()
+        self.assertEqual(max_value, None)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
