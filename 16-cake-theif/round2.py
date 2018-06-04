a12a186ef1bb3be277a60c2ebfb99fcca5fc8d88
richhildebrand <richhildebrand@gmail.com>
2018-06-04 12:57:25 -0400

    working without zero weight cake

--- 16-cake-theif/round2.py
+++ 16-cake-theif/round2.py
@@ -1,16 +1,17 @@
 def max_duffel_bag_value(cake_tuples, capacity):
     max_profits = {}
 
-    for current_capacity in range(1, capacity+1):
+    for current_capacity in range(0, capacity+1):
+        max_profits[current_capacity] = max_profits.get(current_capacity, 0)
 
         for weight, value in cake_tuples:
             if weight > current_capacity: continue
 
             remainder = current_capacity - weight
             potential_profit = value + max_profits[remainder]
-            max_profit = current_capacity.get(current_capacity, 0)
+            max_profit = max_profits[current_capacity]
 
-            if potential_profit > max_profit: max_profits[current_capacity] = max_profit
+            if potential_profit > max_profit: max_profits[current_capacity] = potential_profit
 
 
     return max_profits[capacity]
